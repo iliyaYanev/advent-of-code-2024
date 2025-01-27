0a7f46197b7f8a975ab9511bd5e1cf2ac9f746bf
Iliya Yanev <iliyayanev92@gmail.com>
2025-01-27 18:46:57 +0200

    day 11

--- README.md
+++ README.md
@@ -12,3 +12,4 @@
 * ##### [Resonant Collinearity](https://github.com/iliyaYanev/advent-of-code-2024/tree/master/src/main/java/day_08) **-** [Intersection Algorithm](https://en.wikipedia.org/wiki/Intersection_algorithm)
 * ##### [Disk Fragmenter](https://github.com/iliyaYanev/advent-of-code-2024/tree/master/src/main/java/day_09) **-** [Greedy Best First Search](https://www.geeksforgeeks.org/greedy-best-first-search-algorithm/)
 * ##### [Hoof It](https://github.com/iliyaYanev/advent-of-code-2024/tree/master/src/main/java/day_10) **-** [DFS](https://en.wikipedia.org/wiki/Depth-first_search)
+* ##### [Plutonian Pebbles](https://github.com/iliyaYanev/advent-of-code-2024/tree/master/src/main/java/day_11) **-** [Markov Chain](https://en.wikipedia.org/wiki/Markov_chain)
---
+++ src/main/java/day_11/PlutonianPebbles.java
@@ -0,0 +1,46 @@
+package day_11;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class PlutonianPebbles {
+
+    public static long blinkStones(String input, int blinks) {
+        Map<Long, Long> stones = Arrays.stream(input.split("\\s+"))
+            .map(Long::parseLong)
+            .collect(Collectors.toMap(l -> l, l -> 1L));
+
+        for (int i = 0; i < blinks; i++) {
+            Map<Long, Long> newStones = new HashMap<>();
+
+            stones.forEach((stone, count) ->
+                blink(stone).forEach(newStone -> newStones.merge(newStone, count, Long::sum))
+            );
+
+            stones = newStones;
+        }
+
+        return stones.values().stream().mapToLong(Long::longValue).sum();
+    }
+
+    private static List<Long> blink(long stone) {
+        if (stone == 0L) {
+            return List.of(1L);
+        }
+
+        String stoneStr = Long.toString(stone);
+        int length = stoneStr.length();
+
+        if (length % 2 == 0) {
+            return List.of(
+                Long.parseLong(stoneStr.substring(0, length / 2)),
+                Long.parseLong(stoneStr.substring(length / 2))
+            );
+        }
+
+        return List.of(stone * 2024);
+    }
+}
