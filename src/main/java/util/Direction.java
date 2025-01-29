029be8da51ee37a5825818a417e356f3e30bfbb8
Iliya Yanev <iliyayanev92@gmail.com>
2025-01-29 18:15:14 +0200

    remove not used methods

--- src/main/java/util/Direction.java
+++ src/main/java/util/Direction.java
@@ -23,31 +23,12 @@ public enum Direction {
         };
     }
 
-    public Direction reverse() {
-        return switch (this) {
-            case NORTH -> SOUTH;
-            case SOUTH -> NORTH;
-            case EAST -> WEST;
-            case WEST -> EAST;
-        };
-    }
-
     public static Direction fromChar(char c) {
         return switch (c) {
             case '^' -> NORTH;
             case 'v' -> SOUTH;
             case '<' -> WEST;
             case '>' -> EAST;
-            default -> throw new IllegalArgumentException("Invalid direction: " + c);
-        };
-    }
-
-    public static Direction fromInitial(char c) {
-        return switch (c) {
-            case 'N' -> NORTH;
-            case 'S' -> SOUTH;
-            case 'W' -> WEST;
-            case 'E' -> EAST;
             default -> throw new IllegalArgumentException("Invalid direction: " + c);
         };
     }
